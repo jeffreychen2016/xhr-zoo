6a736125253ba5dcd51cdf215d9e94eb6dc365c1
jeffrey chen <jeffreychen2016@gmail.com>
2018-03-27 18:36:44 -0500

    Adding js code for creating cards

--- main.js
+++ main.js
@@ -5,9 +5,16 @@ const printToDom = (string,id) => {
  const buildDomString = (animals) => {
      let string = '';
      for(let i=0; i<animals.length; i++){
-        string += `<h1>${animals[i].names}</h1>`;
+        string += `<div class='animal'>`;
+        string +=   `<h1>${animals[i].names}</h1>`;
+        string +=   `<h3>${animals[i].number}</h3>`;
+        string +=   `<img class='animal-image' src='${animals[i].imageURL}'>`;
+        string +=   `<div class='button-container'>`;
+        string +=       `<button>Escaped</button>`;
+        string +=   `</div>`;
+        string += `</div>`;
       }
-      printToDom(string, 'card-holder')
+      printToDom(string, 'zoo')
  }
 
 
